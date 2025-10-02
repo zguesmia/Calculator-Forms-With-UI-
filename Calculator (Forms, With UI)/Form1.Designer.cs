9d86c462f6796fa006fc40276d2ea3ff55a1c572
Hassan Guesmia <hguesmia@hotmail.com>
2025-10-02 16:15:47 -0500

    tr

--- Calculator (Forms, With UI)/Form1.Designer.cs
+++ Calculator (Forms, With UI)/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.SquareRoot = new System.Windows.Forms.Button();
             this.SquareRoot3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Multiplication
@@ -146,12 +147,25 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(173, 132);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.One_Click);
+            // 
             // CalculatorApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(507, 391);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.SquareRoot3);
             this.Controls.Add(this.SquareRoot);
@@ -180,6 +194,7 @@
         private System.Windows.Forms.Button SquareRoot;
         private System.Windows.Forms.Button SquareRoot3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
